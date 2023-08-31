696e534c9d40c86b14ef72388fbf86602dcfddeb
Zeyad <ZeyadMohamed.BUS.ACC@Gmail.com>
2023-08-31 10:39:43 +0300

    Added Essential Theme Values

--- src/utils/theme.ts
+++ src/utils/theme.ts
@@ -1,6 +1,37 @@
 // Imports
 import { extendTheme } from "@chakra-ui/react";
 // Add Custom Values
-const theme = extendTheme( { } );
+const theme = extendTheme( { 
+    // Custom Colors
+    colors: {
+        primary: "#FFFFFF",
+        secondary: "#1A1A1A",
+        call_to_action: "#7F56D9",
+        accent_purple_light: "#F9F5FF",
+        accent_purple_dark: "#6941C6",
+        accent_blue_light: "#EEF4FF",
+        accent_blue_dark: "#3538CD",
+        accent_red_light: "#FDF2FA",
+        accent_red_dark: "#C11574",
+        accent_orange_light: "#FFF6ED",
+        accent_orange_dark: "#C4320A",
+        accent_green_light: "#ECFDF3",
+        accent_green_regular: "#21D188",
+        accent_green_dark: "#027A48",
+        accent_gray_light: "#F8F9FC",
+        accent_gray_regular: "#667085",
+        accent_gray_dark: "#363F72",
+    },
+    // Custom Components
+    components: {
+        Heading: {
+            variants: {
+                h1: { fontSize: "36px" },
+                h2: { fontSize: "24px" },
+                h3: { fontSize: "18px" },
+            }
+        }
+    }
+ } );
 // Exports
 export default theme;
